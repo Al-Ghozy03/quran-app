112e0412c13c7de4740d918d051c4660c01ead67
Rifqi Hasan <hasanrifqi0905@gmail.com>
2021-09-13 15:17:45 +0700

    membuat home ui

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,9 +3,7 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class  HomeScreen extends StatelessWidget {
- 
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,28 +16,37 @@ class  HomeScreen extends StatelessWidget {
               Stack(
                 children: [
                   Container(
-                  width: 550.0,
-                  height: 300.0,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(image: NetworkImage("https://image.freepik.com/free-vector/islamic-center-monoline-logo_322595-753.jpg")),
-                    border: Border.all(color: Theme.of(context).primaryColor),
-                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(50.0)),
+                    width: 550.0,
+                    height: 300.0,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://image.freepik.com/free-vector/islamic-center-monoline-logo_322595-753.jpg")),
+                      border: Border.all(color: Theme.of(context).primaryColor),
+                      borderRadius:
+                          BorderRadius.only(bottomRight: Radius.circular(50.0)),
+                    ),
                   ),
-                  ),
-                  
                 ],
               ),
               SizedBox(
                 height: 40,
               ),
-             
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Column(
                     children: [
-                      Image.network("https://image.freepik.com/free-photo/islamic-3d-render-arabic-theme-background-with-alquran-moon-arabic-lamp_462569-98.jpg",height: 150,width: 200,),
-                       SizedBox(
+                      Container(
+                        height: 150,  
+                        width: 250,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    "https://image.freepik.com/free-photo/islamic-3d-render-arabic-theme-background-with-alquran-moon-arabic-lamp_462569-98.jpg",))),
+                      ),
+                      SizedBox(
                         height: 8,
                       ),
                       Text("Alquran")
@@ -47,7 +54,11 @@ class  HomeScreen extends StatelessWidget {
                   ),
                   Column(
                     children: [
-                      Image.network("https://tse1.mm.bing.net/th?id=OIP.twWeQnYiMxSFyf-UrpIfLAHaFj&pid=Api&P=0&w=206&h=156",height: 150,width: 200,),
+                      Image.network(
+                        "https://tse1.mm.bing.net/th?id=OIP.twWeQnYiMxSFyf-UrpIfLAHaFj&pid=Api&P=0&w=206&h=156",
+                        height: 150,
+                        width: 200,
+                      ),
                       SizedBox(
                         height: 8,
                       ),
@@ -59,13 +70,17 @@ class  HomeScreen extends StatelessWidget {
               SizedBox(
                 height: 15,
               ),
-               Row(
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Column(
                     children: [
-                      Image.network("https://image.freepik.com/free-photo/islamic-3d-render-arabic-theme-background-with-alquran-moon-arabic-lamp_462569-98.jpg",height: 150,width: 200,),
-                       SizedBox(
+                      Image.network(
+                        "https://image.freepik.com/free-photo/islamic-3d-render-arabic-theme-background-with-alquran-moon-arabic-lamp_462569-98.jpg",
+                        height: 150,
+                        width: 200,
+                      ),
+                      SizedBox(
                         height: 8,
                       ),
                       Text("Dzikir")
@@ -73,8 +88,12 @@ class  HomeScreen extends StatelessWidget {
                   ),
                   Column(
                     children: [
-                      Image.network("https://image.freepik.com/free-photo/islamic-3d-render-arabic-theme-background-with-alquran-moon-arabic-lamp_462569-98.jpg",height: 150,width: 200,),
-                       SizedBox(
+                      Image.network(
+                        "https://image.freepik.com/free-photo/islamic-3d-render-arabic-theme-background-with-alquran-moon-arabic-lamp_462569-98.jpg",
+                        height: 150,
+                        width: 200,
+                      ),
+                      SizedBox(
                         height: 8,
                       ),
                       Text("alquran")
@@ -82,11 +101,9 @@ class  HomeScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              
             ],
           ),
         ),
-        
       ),
     );
   }
