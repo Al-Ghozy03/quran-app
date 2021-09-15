4117c538f2c98d4d5582e58bf03d168887c3e505
Rifqi Hasan <hasanrifqi0905@gmail.com>
2021-09-15 12:55:29 +0700

    blueprinthadits

---
+++ lib/model/malik.dart
@@ -0,0 +1,79 @@
+class Malik{
+  int? number;
+  String? arab;
+  String? id;
+
+Malik({this.number, this.arab, this.id});
+}
+var dataHadits=[
+   Malik(
+    number : 1,
+    arab : "قَالَ حَدَّثَنِي اللَّيْثِيّ عَنْ مَالِك بْن أَنَس عَنْ ابْنِ شِهَابٍ أَنَّ عُمَرَ بْنَ عَبْدِ الْعَزِيزِ أَخَّرَ الصَّلَاةَ يَوْمًا فَدَخَلَ عَلَيْهِ عُرْوَةُ بْنُ الزُّبَيْرِ فَأَخْبَرَهُ أَنَّ الْمُغِيرَةَ بْنَ شُعْبَةَ أَخَّرَ الصَّلَاةَ يَوْمًا وَهُوَ بِالْكُوفَةِ فَدَخَلَ عَلَيْهِ أَبُو مَسْعُودٍ الْأَنْصَارِيُّ فَقَالَمَا هَذَا يَا مُغِيرَةُ أَلَيْسَ قَدْ عَلِمْتَ أَنَّ جِبْرِيلَ نَزَلَ فَصَلَّى فَصَلَّى رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ ثُمَّ صَلَّى فَصَلَّى رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ ثُمَّ صَلَّى فَصَلَّى رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ ثُمَّ صَلَّى فَصَلَّى رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ ثُمَّ صَلَّى فَصَلَّى رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ ثُمَّ قَالَ بِهَذَا أُمِرْتُ فَقَالَ عُمَرُ بْنُ عَبْدِ الْعَزِيزِ اعْلَمْ مَا تُحَدِّثُ بِهِ يَا عُرْوَةُ أَوَ إِنَّ جِبْرِيلَ هُوَ الَّذِي أَقَامَ لِرَسُولِ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ وَقْتَ الصَّلَاةِقَالَ عُرْوَةُ كَذَلِكَ كَانَ بَشِيرُ بْنُ أَبِي مَسْعُودٍ الْأَنْصَارِيُّ يُحَدِّثُ عَنْ أَبِيهِ قَالَ عُرْوَةُ وَلَقَدْ حَدَّثَتْنِي عَائِشَةُ زَوْجُ النَّبِيِّ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ أَنَّ رَسُولَ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ كَانَ يُصَلِّي الْعَصْرَ وَالشَّمْسُ فِي حُجْرَتِهَا قَبْلَ أَنْ تَظْهَرَ",
+    id : "Perawi berkata; Telah menceritakan kepadaku Al Laitsi dari Malik bin Anas dari [Ibnu Syihab]; Suatu hari Umar bin Abdul Aziz pernah mengakhirkan shalat, maka Urwah bin Az Zubair menemuinya dan memberitahukan kepadanya, bahwa suatu hari Mughirah bin Syu'bah mengakhirkan shalat ketika berada di Kufah, sehingga Ibnu Mas'ud menemuinya dan menegurnya; \"Apa maksudmu, hai Mughirah? bukankah kamu tahu, Jibril telah turun kemudian shalat dan Rasulullah shallallahu 'alaihi wasallam ikut shalat, kemudian dia shalat dan Rasulullah shallallahu 'alaihi wasallam ikut shalat, kemudian dia shalat dan Rasulullah shallallahu 'alaihi wasallam ikut shalat juga, kemudian dia shalat dan Rasulullah shallallahu 'alaihi wasallam ikut shalat. Lalu Jibril berkata; \"Seperti ini aku diperintahkan, \" maka Umar bin Abdul Aziz bertanya, \"perhatikanlah apa yang kamu riwayatkan hai Urwah! Apakah Jibril yang mengajarkan waktu shalat untuk Rasulullah shallallahu 'alaihi wasallam?\" [Urwah] menjawab; \" [Basyir bin Mas'ud al Anshari] menceritakan dari [Bapaknya] seperti itu juga\" kemudian [Urwah] menegaskan; dan telah menceritakan kepadaku [Aisyah], istri Nabi shallallahu 'alaihi wasallam, bahwa Rasulullah shallallahu 'alaihi wasallam shalat ashar ketika matahari masih di tempatnya belum tampak. "
+  ),
+  Malik(
+    number : 2,
+    arab : "و حَدَّثَنِي يَحْيَى عَنْ مَالِك عَنْ زَيْدِ بْنِ أَسْلَمَ عَنْ عَطَاءِ بْنِ يَسَارٍأَنَّهُ قَالَ جَاءَ رَجُلٌ إِلَى رَسُولِ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ فَسَأَلَهُ عَنْ وَقْتِ صَلَاةِ الصُّبْحِ قَالَ فَسَكَتَ عَنْهُ رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ حَتَّى إِذَا كَانَ مِنْ الْغَدِ صَلَّى الصُّبْحَ حِينَ طَلَعَ الْفَجْرُ ثُمَّ صَلَّى الصُّبْحَ مِنْ الْغَدِ بَعْدَ أَنْ أَسْفَرَ ثُمَّ قَالَ أَيْنَ السَّائِلُ عَنْ وَقْتِ الصَّلَاةِ قَالَ هَأَنَذَا يَا رَسُولَ اللَّهِ فَقَالَ مَا بَيْنَ هَذَيْنِ وَقْتٌ",
+    id : "Telah menceritakan kepadaku Yahya dari Malik dari [Zaid bin Aslam] dari ['Atho` bin Yasar], dia berkata; seorang laki-laki datang kepada Rasulullah shallallahu 'alaihi wasallam lalu bertanya tentang waktu shalat subuh. ('Atho` bin Yasar) berkata; lalu Rasulullah shallallahu 'alaihi wasallam diam, hingga esok harinya beliau shalat subuh ketika terbit fajar, besoknya beliau shalat ketika langit telah menguning. lalu (Rasulullah shallallahu 'alaihi wasallam) bertanya: \"Dimana laki-laki yang bertanya kepadaku tentang waktu shalat subuh?\" (Laki-laki itu) menjawab; \"Saya, Wahai Rasulullah, \" maka beliau bersabda: \" waktu shalat subuh diantara dua waktu ini.\""
+  ),
+  Malik(
+    number : 3,
+    arab : "و حَدَّثَنِي يَحْيَى عَنْ مَالِك عَنْ يَحْيَى بْنِ سَعِيدٍ عَنْ عَمْرَةَ بِنْتِ عَبْدِ الرَّحْمَنِ عَنْ عَائِشَةَ زَوْجِ النَّبِيِّ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ أَنَّهَا قَالَتْإِنْ كَانَ رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ لَيُصَلِّي الصُّبْحَ فَيَنْصَرِفُ النِّسَاءُ مُتَلَفِّعَاتٍ بِمُرُوطِهِنَّ مَا يُعْرَفْنَ مِنْ الْغَلَسِ",
+    id : "Telah menceritakan kepadaku Yahya dari Malik dari [Yahya bin Sa'id] dari ['Amrah binti Abdurrahman] dari [Aisyah] istri Nabi shallallahu 'alaihi wasallam dia berkata; \"ketika Rasulullah shallallahu 'alaihi wasallam selesai shalat subuh, kaum wanita keluar dengan keadaan tertutup dengan kain mereka, dan mereka masih tidak terinditifikasi karena gelap.\""
+  ),
+  Malik(
+    number : 4,
+    arab : "و حَدَّثَنِي عَنْ مَالِك عَنْ زَيْدِ بْنِ أَسْلَمَ عَنْ عَطَاءِ بْنِ يَسَارٍ وَعَنْ بُسْرِ بْنِ سَعِيدٍ وَعَنْ الْأَعْرَجِ كُلُّهُمْ يُحَدِّثُونَهُ عَنْ أَبِي هُرَيْرَةَأَنَّ رَسُولَ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ قَالَ مَنْ أَدْرَكَ رَكْعَةً مِنْ الصُّبْحِ قَبْلَ أَنْ تَطْلُعَ الشَّمْسُ فَقَدْ أَدْرَكَ الصُّبْحَ وَمَنْ أَدْرَكَ رَكْعَةً مِنْ الْعَصْرِ قَبْلَ أَنْ تَغْرُبَ الشَّمْسُ فَقَدْ أَدْرَكَ الْعَصْرَ",
+    id : "Telah menceritakan kepadaku dari Malik dari [Zaid bin Aslam] dari ['Atho` bin Yasar], [Busr bin Said] dan [Al A'raj], semuanya menceritakannya dari [Abu Hurairah], Rasulullah shallallahu 'alaihi wasallam bersabda: \"Barangsiapa mendapatkan satu raka'at shalat subuh sebelum matahari terbit, dia telah mendapatkan shalat subuh. Barangsiapa mendapatkan satu raka'at shalat Ashar sebelum matahari terbenam maka dia telah mendapatkan shalat ashar.\""
+  ),
+  Malik(
+    number : 5,
+    arab : "و حَدَّثَنِي عَنْ مَالِك عَنْ نَافِعٍ مَوْلَى عَبْدِ اللَّهِ بْنِ عُمَرَأَنَّ عُمَرَ بْنَ الْخَطَّابِ كَتَبَ إِلَى عُمَّالِهِ إِنَّ أَهَمَّ أَمْرِكُمْ عِنْدِي الصَّلَاةُ فَمَنْ حَفِظَهَا وَحَافَظَ عَلَيْهَا حَفِظَ دِينَهُ وَمَنْ ضَيَّعَهَا فَهُوَ لِمَا سِوَاهَا أَضْيَعُ ثُمَّ كَتَبَ أَنْ صَلُّوا الظُّهْرَ إِذَا كَانَ الْفَيْءُ ذِرَاعًا إِلَى أَنْ يَكُونَ ظِلُّ أَحَدِكُمْ مِثْلَهُ وَالْعَصْرَ وَالشَّمْسُ مُرْتَفِعَةٌ بَيْضَاءُ نَقِيَّةٌ قَدْرَ مَا يَسِيرُ الرَّاكِبُ فَرْسَخَيْنِ أَوْ ثَلَاثَةً قَبْلَ غُرُوبِ الشَّمْسِ وَالْمَغْرِبَ إِذَا غَرَبَتْ الشَّمْسُ وَالْعِشَاءَ إِذَا غَابَ الشَّفَقُ إِلَى ثُلُثِ اللَّيْلِ فَمَنْ نَامَ فَلَا نَامَتْ عَيْنُهُ فَمَنْ نَامَ فَلَا نَامَتْ عَيْنُهُ فَمَنْ نَامَ فَلَا نَامَتْ عَيْنُهُ وَالصُّبْحَ وَالنُّجُومُ بَادِيَةٌ مُشْتَبِكَةٌ",
+    id : "Telah menceritakan kepadaku dari Malik dari [Nafi'] mantan budak Abdullah bin Umar; [Umar bin Al Khatthab] pernah menulis surat kepada para pegawainya, \"Menurutku yang paling penting dari urusan kalian adalah shalat. Barangsiapa yang menjaganya dan memeliharanya maka dia telah menjaga agamanya. Barangsiapa menyia-nyiakannya, maka amalan yang lainnya akan lebih terabaikan, \" lalu dia menulis; \"Hendaklah kalian shalat zhuhur ketika bayangan kalian sehasta hingga bayangan kalian seukuran persis dengan tubuh kalian. Hendaklah kalian shalat ashar ketika matahari masih tinggi dan putih jernih dengan ukuran perjalanan berkendara sekitar dua atau tiga farsakh sebelum matahari terbenam. laksanakanlah shalat maghrib ketika terbenamnya matahari, dan isya` ketika syafaq (mega merah di langit setelah terbenamnya matahari) telah hilang hingga sepertiga malam. Barangsiapa yang tidur, maka tidak terlelap matanya, barangsiapa yang tidur, tidak terlelap matanya. Dan laksanakanlah shalat subuh ketika bintang-bintang terlihat saling bercampur.\""
+  ),
+  Malik(
+    number : 6,
+    arab : "و حَدَّثَنِي عَنْ مَالِك عَنْ عَمِّهِ أَبِي سُهَيْلٍ عَنْ أَبِيهِأَنَّ عُمَرَ بْنَ الْخَطَّابِ كَتَبَ إِلَى أَبِي مُوسَى أَنْ صَلِّ الظُّهْرَ إِذَا زَاغَتْ الشَّمْسُ وَالْعَصْرَ وَالشَّمْسُ بَيْضَاءُ نَقِيَّةٌ قَبْلَ أَنْ يَدْخُلَهَا صُفْرَةٌ وَالْمَغْرِبَ إِذَا غَرَبَتْ الشَّمْسُ وَأَخِّرْ الْعِشَاءَ مَا لَمْ تَنَمْ وَصَلِّ الصُّبْحَ وَالنُّجُومُ بَادِيَةٌ مُشْتَبِكَةٌ وَاقْرَأْ فِيهَا بِسُورَتَيْنِ طَوِيلَتَيْنِ مِنْ الْمُفَصَّلِ",
+    id : "Telah menceritakan kepadaku dari Malik dari pamannya - [Abu Suhail] - dari [bapaknya], bahwa [Umar bin Al Khathab] menulis kepada Abu Musa Al Asya'ri: laksanakanlah shalat zhuhur ketika matahari tergelincir dan shalat ashar ketika matahari terlihat putih jernih sebelum menguning. Laksanakanlah Shalat maghrib ketika matahari terbenam, dan akhirkan shalat Isya' selagi kamu belum tidur, dan laksanakanlah shalat subuh ketika bintang-bintang terlihat saling bersusun, dan bacalah di dalamnya dua surat panjang yang termasuk kategori dari surat-surat Al Mufasshal (surat yang dimulai dari surat Al Hujurat sampai akhir) . "
+  ),
+  Malik(
+    number : 7,
+    arab : "و حَدَّثَنِي عَنْ مَالِك عَنْ هِشَامِ بْنِ عُرْوَةَ عَنْ أَبِيهِأَنَّ عُمَرَ بْنَ الْخَطَّابِ كَتَبَ إِلَى أَبِي مُوسَى الْأَشْعَرِيِّ أَنْ صَلِّ الْعَصْرَ وَالشَّمْسُ بَيْضَاءُ نَقِيَّةٌ قَدْرَ مَا يَسِيرُ الرَّاكِبُ ثَلَاثَةَ فَرَاسِخَ وَأَنْ صَلِّ الْعِشَاءَ مَا بَيْنَكَ وَبَيْنَ ثُلُثِ اللَّيْلِ فَإِنْ أَخَّرْتَ فَإِلَى شَطْرِ اللَّيْلِ وَلَا تَكُنْ مِنْ الْغَافِلِينَ",
+    id : "Telah menceritakan kepadaku dari Malik dari [Hisyam bin Urwah] dari [bapaknya]; [Umar bin Al Khatthab] pernah menulis kepada Abu Musa Al Asy'ari, bahwa laksanakanlah shalat ashar ketika matahari terlihat putih jernih dengan kadar perjalanan tiga farsakh, dan laksanakanlah shalat Isya' antara kamu shalat hingga sepertiga malam, jika kamu ingin mengakhirkannya maka hingga separuh malam dan janganlah termasuk orang-orang yang lalai. "
+  ),
+  Malik(
+    number : 8,
+    arab : "و حَدَّثَنِي عَنْ مَالِك عَنْ يَزِيدَ بْنِ زِيَادٍ عَنْ عَبْدِ اللَّهِ بْنِ رَافِعٍ مَوْلَى أُمِّ سَلَمَةَ زَوْجِ النَّبِيِّ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَأَنَّهُ سَأَلَ أَبَا هُرَيْرَةَ عَنْ وَقْتِ الصَّلَاةِ فَقَالَ أَبُو هُرَيْرَةَ أَنَا أُخْبِرُكَ صَلِّ الظُّهْرَ إِذَا كَانَ ظِلُّكَ مِثْلَكَ وَالْعَصْرَ إِذَا كَانَ ظِلُّكَ مِثْلَيْكَ وَالْمَغْرِبَ إِذَا غَرَبَتْ الشَّمْسُ وَالْعِشَاءَ مَا بَيْنَكَ وَبَيْنَ ثُلُثِ اللَّيْلِ وَصَلِّ الصُّبْحَ بِغَبَشٍ يَعْنِي الْغَلَسَ",
+    id : "Telah menceritakan kepadaku dari Malik dari [Yazid bin Ziyad] dari [Abdullah bin Rafi'] mantan budak Ummu Salamah, istri Nabi shallallahu 'alaihi wasallam, dia bertanya kepada Abu Hurairah tentang waktu shalat. Maka [Abu Hurairah] berkata; saya akan menjelaskannya kepadamu, (yaitu;) laksanakanlah shalat zhuhur ketika bayanganmu persis seperti tinggimu, dan shalat ashar ketika bayanganmu menjadi dua kali lipat lebih panjang, shalat maghrib ketika matahari terbenam dan Isya` diantara kamu shalat dengan sepertiga malam. Dan laksanakanlah shalat subuh di akhir malam masih suasana gelap. "
+  ),
+  Malik(
+    number : 10,
+    arab : "و حَدَّثَنِي عَنْ مَالِك عَنْ ابْنِ شِهَابٍ عَنْ أَنَسِ بْنِ مَالِكٍ أَنَّهُ قَالَكُنَّا نُصَلِّي الْعَصْرَ ثُمَّ يَذْهَبُ الذَّاهِبُ إِلَى قُبَاءٍ فَيَأْتِيهِمْ وَالشَّمْسُ مُرْتَفِعَةٌ",
+    id : "Telah menceritakan kepadaku dari dari Malik dari [Ibnu Syihab] dari [Anas bin Malik], dia berkata; \"kami shalat asar, lalu salah seorang dari kami pergi menuju Quba`, dan sesampainya di sana matahari masih terlihat tinggi.\""
+  ),
+  Malik(
+    number : 11,
+    arab : "و حَدَّثَنِي عَنْ مَالِك عَنْ رَبِيعَةَ بْنِ أَبِي عَبْدِ الرَّحْمَنِ عَنْ الْقَاسِمِ بْنِ مُحَمَّدٍ أَنَّهُ قَالَمَا أَدْرَكْتُ النَّاسَ إِلَّا وَهُمْ يُصَلُّونَ الظُّهْرَ بِعَشِيٍّ",
+    id : "Telah menceritakan kepadaku dari Malik dari [Rabi'ah bin Abu Abdurrahman] dari [Al Qasim bin Muhammad], dia berkata; \"Aku tidak mendapati orang-orang, kecuali mereka sedang mengerjakan shalat zhuhur di waktu 'asyiy.\" "
+  ),
+  Malik(
+    number : 12,
+    arab : "حَدَّثَنِي يَحْيَى عَنْ مَالِك عَنْ عَمِّهِ أَبِي سُهَيْلِ بْنِ مَالِكٍ عَنْ أَبِيهِ أَنَّهُ قَالَكُنْتُ أَرَى طِنْفِسَةً لِعَقِيلِ بْنِ أَبِي طَالِبٍ يَوْمَ الْجُمُعَةِ تُطْرَحُ إِلَى جِدَارِ الْمَسْجِدِ الْغَرْبِيِّ فَإِذَا غَشِيَ الطِّنْفِسَةَ كُلَّهَا ظِلُّ الْجِدَارِ خَرَجَ عُمَرُ بْنُ الْخَطَّابِ وَصَلَّى الْجُمُعَةَقَالَ مَالِكٌ ثُمَّ نَرْجِعُ بَعْدَ صَلَاةِ الْجُمُعَةِ فَنَقِيلُ قَائِلَةَ الضَّحَاءِ",
+    id : "Telah menceritakan kepadaku Yahya dari Malik dari pamannya - [Abu Suhail bin Malik] - dari [bapaknya], dia berkata; \"Saya melihat permadani milik 'Uqail bin Abu Thalib pada Hari Jum'at diletakkan di dinding masjid arah barat, maka ketika bayang-bayang permadani tersebut telah menutupi seluruh permadani, [Umar bin Al Khatthab] keluar dan shalat Jum'at.\" Malik berkata; \"Lalu kami pulang setelah shalat Jum'at dan tidur qailulah di waktu siang.\" "
+  ),
+  Malik(
+    number : 13,
+    arab : "و حَدَّثَنِي عَنْ مَالِك عَنْ عَمْرِو بْنِ يَحْيَى الْمَازِنِيِّ عَنْ ابْنِ أَبِي سَلِيطٍ أَنَّ عُثْمَانَ بْنَ عَفَّانَصَلَّى الْجُمُعَةَ بِالْمَدِينَةِ وَصَلَّى الْعَصْرَ بِمَلَلٍقَالَ مَالِك وَذَلِكَ لِلتَّهْجِيرِ وَسُرْعَةِ السَّيْرِ",
+    id : "Telah menceritakan kepadaku dari Malik dari ['Amru bin Yahya Al Mazini] dari [Ibnu Abu Salith], bahwa [Utsman bin 'Affan] shalat Jum'at di Madinah dan shalat ashar di Malal, yang demikian itu untuk menyegerakan shalat jum'at dan mempercepat perjalanan.\""
+  ),
+  Malik(
+    number : 14,
+    arab : "حَدَّثَنِي يَحْيَى عَنْ مَالِك عَنْ ابْنِ شِهَابٍ عَنْ أَبِي سَلَمَةَ بْنِ عَبْدِ الرَّحْمَنِ عَنْ أَبِي هُرَيْرَةَأَنَّ رَسُولَ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ قَالَ مَنْ أَدْرَكَ رَكْعَةً مِنْ الصَّلَاةِ فَقَدْ أَدْرَكَ الصَّلَاةَ",
+    id : "Telah menceritakan kepadaku Yahya dari Malik dari [Ibnu Syihab] dari [Abu Salamah bin Abdurrahman] dari [Abu Hurairah] Rasulullah shallallahu 'alaihi wasallam bersabda: \"Barangsiapa mendapatkan satu raka'at shalat, maka dia telah mendapatkan shalat tersebut.\""
+  ),
+  Malik(
+    number : 15,
+    arab : "و حَدَّثَنِي عَنْ مَالِك عَنْ نَافِعٍ أَنَّ عَبْدَ اللَّهِ بْنَ عُمَرَ بْنِ الْخَطَّابِ كَانَ يَقُولُإِذَا فَاتَتْكَ الرَّكْعَةُ فَقَدْ فَاتَتْكَ السَّجْدَةُ",
+    id : "Telah menceritakan kepadaku dari Malik dari [Nafi'] dari [Abdullah bin Umar bin Khathab] berkata; \"Apabila kamu tertinggal dari ruku' maka kamu telah ketinggalan satu raka'at.\""
+  ),
+];
