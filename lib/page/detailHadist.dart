8063091cd66d416e87ce5a479fe93f2b733079e7
junajunior <muhammmadarjuna24@gmail.com>
2021-09-21 12:20:32 +0700

    detail hadis

--- lib/page/detailHadist.dart
+++ lib/page/detailHadist.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:quran_app/model/bukhari.dart';
 
 class DetailHadist extends StatelessWidget {
-  final Bukhari hadist;
+  final Hadist hadist;
   DetailHadist({required this.hadist});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Row(
-          children: [Text(hadist.arab)],
+        child: Column(
+          children: [
+            Text(hadist.arab.toString()),
+            Text(hadist.id.toString()),
+            ],
         ),
       ),
     );
